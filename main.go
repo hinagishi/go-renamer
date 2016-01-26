b0cdaea67190a3963c4fad737490806bc144a055
hinagishi <archangel.ratziel@gmail.com>
2016-01-26 18:04:13 +0900

    add a function to rename all file in a specified directory

--- main.go
+++ main.go
@@ -3,11 +3,55 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func usage() {
 	fmt.Println("Usage: go-renamer filepath")
+}
+
+func renameAll(f string) {
+	if !strings.HasSuffix(f, "/") {
+		f += "/"
+	}
+	files, err := ioutil.ReadDir(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	i := 0
+	newfiles := make([]string, len(files))
+	for p, n := range files {
+		if n.Name()[0] == '.' {
+			newfiles[p] = n.Name()
+		} else if strings.Index(n.Name(), ".") == -1 {
+			newfiles[p] = fmt.Sprintf("%03d", i)
+			i++
+		} else {
+			tmp := strings.Split(n.Name(), ".")
+			suffix := tmp[len(tmp)-1]
+			newfiles[p] = fmt.Sprintf("%03d.%s", i, suffix)
+			i++
+		}
+	}
+
+	for i := 0; i < len(newfiles); i++ {
+		fmt.Println(files[i].Name() + " --> " + newfiles[i])
+	}
+	fmt.Print("Really change files name?(y/n): ")
+	reader := bufio.NewReader(os.Stdin)
+	c, err := reader.ReadByte()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if c == 'y' {
+		for i := 0; i < len(newfiles); i++ {
+			os.Rename(f+files[i].Name(), f+newfiles[i])
+		}
+	}
 }
 
 func main() {
@@ -23,6 +67,7 @@ func main() {
 	}
 
 	if f.IsDir() {
+		renameAll(os.Args[1])
 	} else {
 		scanner := bufio.NewScanner(os.Stdin)
 		fmt.Print(f.Name() + "--> ")
